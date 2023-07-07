e83fea02f251e1b45ffba985f5e9fb63f7245554
Samuel Ting <105273734+tingjs05@users.noreply.github.com>
2023-07-08 00:17:22 +0800

    camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // game object to follow
+    public GameObject targetObject;
+    public float followSpeed = 2.5f;
+
+    // define variables
+    private Vector3 targetPosition;
+
+    void Update()
+    {
+        // update target position
+        targetPosition = new Vector3(targetObject.transform.position.x, targetObject.transform.position.y, transform.position.z);
+
+        // move camera if not in target position
+        if (transform.position != targetPosition)
+        {
+            transform.position = Vector3.Slerp(transform.position, targetPosition, followSpeed * Time.deltaTime);
+        }
+    }
+}
